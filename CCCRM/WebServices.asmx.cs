35d9892a2fe684978b11ceb7f7e11bc1bbf2806f
adrianypc <adrianypc@codistri.com>
2016-03-16 21:17:15 +0630

    Working on web service methods for cross-domain

--- CCCRM/WebServices.asmx.cs
+++ CCCRM/WebServices.asmx.cs
@@ -13,7 +13,7 @@ namespace CCCRM
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
+    [System.Web.Script.Services.ScriptService]
     public class WebServices : System.Web.Services.WebService
     {
         [WebMethod]
@@ -205,5 +205,6 @@ namespace CCCRM
             return returnMessage;
 
         }
+
     }
 }
